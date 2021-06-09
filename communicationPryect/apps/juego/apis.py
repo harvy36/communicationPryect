1a7481e6f1ef2bce763c544062c08a995572d2e3
harvy36 <31890877+harvy36@users.noreply.github.com>
2021-06-08 19:23:35 -0500

    crear score

--- communicationPryect/apps/juego/apis.py
+++ communicationPryect/apps/juego/apis.py
@@ -3,12 +3,21 @@ from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveUpdate
 from .serializers import (ScoreSerializer, StatisticSerializer)
 # models
 from .models import (Score, Statistic)
+from rest_framework.authtoken.models import Token
 
 # score
 class scoreCreateAPIView(CreateAPIView):
     queryset = Score.objects.all()
     serializer_class = ScoreSerializer
 
+    def perform_create(self, serializer):
+        try:
+            token = self.request.META['Authorization']
+            token = token.split()
+            tokenobject = Token.object.get(key= token[1])
+            serializer.save(user=tokenobject.user)
+        except:
+            serializer.save()
 
 class scoreListAPIView(ListAPIView):
     queryset = Score.objects.all()
